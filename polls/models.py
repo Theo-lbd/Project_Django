c79ef6095e47ff087ed25f47c7b526a6de07fdaf
Theo Labadie <theo.labadie@fms-ea.com>
2024-10-10 11:31:43 +0200

    ajout de test et débug

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,15 @@
-from django.db import models
 import datetime
-
 from django.db import models
 from django.utils import timezone
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+
+    def __repr__(self):
+        return "<Question: {}>".format(self.question_text)
+
     def __str__(self):
         return self.question_text
 
@@ -17,9 +20,16 @@ class Question(models.Model):
         return self.choice_set.all()
 
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+
